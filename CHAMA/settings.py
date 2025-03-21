24431c76c1bf233727bc78f5e5010c1a32c0c0b5
Tess <wanzatess@students.uonbi.ac.ke>
2025-03-21 12:11:48 +0300

    Fixed app import issue

--- CHAMA/settings.py
+++ CHAMA/settings.py
@@ -27,7 +27,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'HIFACHAMA',
+    'hifachama',
     'django_extensions',
     'rest_framework.authtoken',
     'django_otp',
