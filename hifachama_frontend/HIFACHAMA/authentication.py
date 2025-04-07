c022a6cf6c78b8c153bcc39431e628f3ba9a2097
Tess <wanzatess@students.uonbi.ac.ke>
2025-04-07 13:50:39 +0300

    Cleaned up frontend folder

--- hifachama_frontend/HIFACHAMA/authentication.py
+++
@@ -1,5 +0,0 @@
-from django.contrib.auth import authenticate
-
-def authenticate_user(identifier, password):
-    """Authenticate using username or email"""
-    return authenticate(username=identifier, password=password)
